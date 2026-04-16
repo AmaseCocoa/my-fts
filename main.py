007ca6842a0546f49d11b6f9b1b9df6a6accfbd9
AmaseCocoa <cocoa@amase.cc>
2026-04-17 01:15:43 +0900

    fix: add head endpoint for /

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from src import get_fts
 
 app = FastAPI()
@@ -9,6 +9,10 @@ fts = get_fts()
 with open("./assets/index.html", "r") as f:
     index_html = f.read()
 
+@app.head("/")
+def health():
+    return Response(headers={"X-Status": "healthy"})
+    
 @app.get("/")
 def index():
     return HTMLResponse(index_html)
