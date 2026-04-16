434233546937075a94da90cad507f10b9030b956
AmaseCocoa <cocoa@amase.cc>
2026-04-16 22:47:54 +0900

    chore: とりあえずインデックスを空に

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from src import SimpleFTS
 
 
 fts = SimpleFTS()
-to_index = [
-    "最新のiPhoneで、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック",
-    "最新のAndroidで、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック",
-    "最新のGalaxy Note 7で、あそこの美しい風景を楽しく撮影しました。Galaxyはとても便利です。 #全文検索ロジック",
-    "最新のGalaxy S26で、あそこの美しい風景を楽しく撮影しました。Galaxyはとても便利です。 #全文検索ロジック",
-    "iPhoneの最新ロジック"
-]
+to_index = []
 
 for text in to_index:
     fts.add_index(text)
