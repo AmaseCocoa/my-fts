52d39228f88c4e146ab51cb543d03e6e03de6b82
AmaseCocoa <cocoa@amase.cc>
2026-04-16 21:10:26 +0900

    feat: カウントを追加してみる

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ class SimpleFTS:
 
     def __init__(self) -> None:
         self.__tagger = Tagger()
-        self.__index: dict[str, set] = {}
+        self.__index: dict[str, set[tuple[int, int]]] = {}
         self.__results = {}
         self.__lock = threading.RLock()
+        
+    def __update_count(self, data_list: set[tuple[int, int]], target_id: int) -> set[tuple[int, int]]:
+        d = dict(data_list)
+        d[target_id] = d.get(target_id, 0) + 1
+        return set(d.items())
 
     def __tokenize(self, text: str) -> list[str]:
         result = []
         for word in self.__tagger(text):
+            if text == "最新のGalaxy":
+                print(word)
             orig_text = word.surface
             fmt_text = word.feature.lemma
             pos1 = word.feature.pos1
@@ -33,7 +40,7 @@ class SimpleFTS:
             texts_id = len(self.__results)
             self.__results[len(self.__results)] = text
             for text_to_index in self.__tokenize(text):
-                self.__index.setdefault(text_to_index, set()).add(texts_id)
+                self.__index[text_to_index] = self.__update_count(self.__index.setdefault(text_to_index, set()), texts_id)
             self.__results.update(self.__results)
 
     def search(self, text: str) -> list[str]:
@@ -45,30 +52,23 @@ class SimpleFTS:
         if not targets: 
             return []
 
-        for id in set.intersection(*[set(self.__index.get(k, set())) for k in targets]):
+        for id, count in set.intersection(*[set(self.__index.get(k, set())) for k in targets]):
+            print(count)
             final_result.append(self.__results[id])
         return final_result
 
 
 fts = SimpleFTS()
-
-fts.add_index(
-    "最新のiPhoneで、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック"
-)
-fts.add_index(
-    "最新のAndroidで、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック"
-)
-fts.add_index(
-    "最新のGalaxy Note 7で、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック"
-)
-fts.add_index(
+to_index = [
+    "最新のiPhoneで、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック",
+    "最新のAndroidで、あそこの美しい風景を楽しく撮影しました。 #全文検索ロジック",
+    "最新のGalaxy Note 7で、あそこの美しい風景を楽しく撮影しました。Galaxyはとても便利です。 #全文検索ロジック",
     "iPhoneの最新ロジック"
-)
+]
+
+for text in to_index:
+    fts.add_index(text)
 
 print(fts.search(
-    "Galaxy 7"
-))
-
-print(fts.search(
-    "あ、"
+    "最新のGalaxy"
 ))
